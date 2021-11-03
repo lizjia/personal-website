350525de1330a7dcd3cb512d658f45c8d6e5e00a
lizjia <lisa.jia01@gmail.com>
2021-11-03 01:14:00 -0400

    initial commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,6 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
@@ -20,7 +19,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Lisa Jia Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -31,7 +30,7 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-						   <span>{street}<br />
+						   <span>
 						         {city} {state}, {zip}
                    </span><br />
 						   <span>{phone}</span><br />
